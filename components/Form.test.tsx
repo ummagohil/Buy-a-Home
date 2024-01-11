ef96b1f4eae2c228f5040fa1e45546f9a481d3bd
ummagohil <umgo0510@gmail.com>
2024-01-11 14:54:59 +0000

    test set up

---
+++ components/Form.test.tsx
@@ -0,0 +1,26 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import test, { it } from "node:test";
+import Form from "./Form";
+import { Form as FormProps } from "@/lib/types";
+
+const FormProps: FormProps = {
+  income: 0,
+  onChangeIncome: 0,
+  multiplier: 0,
+  onChangeMultiplier: 0,
+  depositPercentage: 0,
+  onChangeDepositPercentage: 0,
+  mortgageInterestRate: 0,
+  onChangeMortgageInterestRate: 0,
+  mortgageLengthValue: "test",
+  mortgageLengthData: 4,
+  getMortgageLengthText: 0,
+};
+
+test("render elements on the form page", async () => {
+  it("render the input", () => {
+    render(<Form {...FormProps} />);
+    expect(screen.getByRole("input")).toHaveLength(2);
+  });
+});
